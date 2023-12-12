dc92324710ba1b54f6fab39223c964ea475720b6
Ray Brown <raybrown.dev@gmail.com>
2023-12-12 17:09:49 +0000

    display images in 3 columns

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Feed.css'
 
 const Feed = () => {
 
@@ -6,10 +7,23 @@ const Feed = () => {
         <>
         <div>
             <h1>This is the Feed</h1>
-            <img src="https://images.unsplash.com/photo-1617925357736-8a4ea869b800?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="A bald man gazes sideways, his head cleanly shaved, appearing lost in thought or observing something." />            
+            <div class="grid-container">
+                <div class="grid-item"><img src="https://images.unsplash.com/photo-1617925357736-8a4ea869b800?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="A bald man gazes sideways, his head cleanly shaved, appearing lost in thought or observing something." />   </div>
+                <div class="grid-item"><img src="https://images.unsplash.com/photo-1617925357736-8a4ea869b800?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="A bald man gazes sideways, his head cleanly shaved, appearing lost in thought or observing something." />   </div>
+                <div class="grid-item"><img src="https://images.unsplash.com/photo-1617925357736-8a4ea869b800?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="A bald man gazes sideways, his head cleanly shaved, appearing lost in thought or observing something." />   </div>  
+                <div class="grid-item"><img src="https://images.unsplash.com/photo-1617925357736-8a4ea869b800?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="A bald man gazes sideways, his head cleanly shaved, appearing lost in thought or observing something." />   </div>
+                <div class="grid-item"><img src="https://images.unsplash.com/photo-1617925357736-8a4ea869b800?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="A bald man gazes sideways, his head cleanly shaved, appearing lost in thought or observing something." />   </div>
+                <div class="grid-item"><img src="https://images.unsplash.com/photo-1617925357736-8a4ea869b800?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="A bald man gazes sideways, his head cleanly shaved, appearing lost in thought or observing something." />   </div>  
+                <div class="grid-item"><img src="https://images.unsplash.com/photo-1617925357736-8a4ea869b800?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="A bald man gazes sideways, his head cleanly shaved, appearing lost in thought or observing something." />   </div>
+                <div class="grid-item"><img src="https://images.unsplash.com/photo-1617925357736-8a4ea869b800?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="A bald man gazes sideways, his head cleanly shaved, appearing lost in thought or observing something." />   </div>
+                <div class="grid-item"><img src="https://images.unsplash.com/photo-1617925357736-8a4ea869b800?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="A bald man gazes sideways, his head cleanly shaved, appearing lost in thought or observing something." />   </div>  
+                </div>      
         </div>
         </>
     )
 };
+
+// mao each item from mongodb
+//
 
 export default Feed;
