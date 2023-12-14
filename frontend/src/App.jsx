1ae8c8bc676135c30421fc482e560519caf33826
Zahra Hassan <zahrajeylani@icloud.com>
2023-12-14 11:53:47 +0000

    navBar not finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,24 @@
 // This is where our routes should go - I have deleted the boilerplate content
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
+import NavBar from './components/NavBar';
 
 function App() {
 
   return (
     <>
-      <Router>
-{/* Any component put here will appear globally, for example the Navbar may go here. */}
+    
 
+{/* Any component put here will appear globally, for example the Navbar may go here. */}
+      <NavBar navigate={useNavigate()} />
           <Routes>
             <Route path="/" element={<Home />} />
 {/* All other Routes Go Here */}
 
 
           </Routes>
-      </Router>
+
     </>
   );
 }
