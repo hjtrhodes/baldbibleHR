37bab752bd7e64ebc29cae1e83ba3ae1ae778dcb
Zahra Hassan <zahrajeylani@icloud.com>
2023-12-14 12:28:42 +0000

    navBar update but not finished

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
     return (
         <>
         <div>
-            <h1>Home Page:</h1>
-            <h1>Bald Bible</h1>
+            
+            {/* <h1>Bald Bible</h1> */}
             <p>Social network for people who are bald or experiencing hair loss</p>
             <Feed />
         </div>
