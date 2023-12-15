e7dfa52a9716c5e132fafdd6a2d7dc5142571960
your name <your email>
2023-12-15 12:23:47 +0000

    comment the GetImage component in Feed

--- frontend/src/components/feed/Feed.jsx
+++ frontend/src/components/feed/Feed.jsx
@@ -93,7 +93,7 @@ const Feed = () => {
             </ImageListItem>
             ))}
         </ImageList>
-        <GetImages />
+        {/* <GetImages /> */}
         </Box>
         </div>
     );
