7411e285746d44abbf43f5adae9f27dbe607bf51
Zahra Hassan <zahrajeylani@icloud.com>
2023-12-15 16:33:12 +0000

    comment modal

---
+++ backend/models/comment.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const commentSchema = mongoose.Schema({
+    content: { type: String, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    imageId: { type: mongoose.Schema.Types.ObjectId, ref: 'Images', required: true },
+    // created: { type: Date, default: Date.now },
+
+}, { timestamps: true });
+
+// timestamp is better because it tells you when the comment was created or edited.
+module.exports = mongoose.model('Comment', commentSchema);
