b9ecf06d3f91afb8ff53975012e7bef5d8cc5c8a
MichaelWood <mgwood22@sky.com>
2024-01-08 14:08:49 +0000

    styling changes

--- frontend/src/pages/photo upload/Upload.jsx
+++ frontend/src/pages/photo upload/Upload.jsx
@@ -1,11 +1,11 @@
-import react from 'react'
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import react from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Upload = () => {
   const [file, setFile] = useState("");
   const [image, setImage] = useState("");
-  const [uploadedIMG, setUpload] = useState("")
+  const [uploadedIMG, setUpload] = useState("");
 
   useEffect(() => {
     console.log(image);
@@ -19,37 +19,42 @@ const Upload = () => {
       setImage(reader.result);
     };
   };
-  const handleChange = (e) => { 
+  const handleChange = (e) => {
     const file = e.target.files[0];
     setFile(file);
     previewFiles(file);
-  }
-  
-  const handleSubmit = async(e) => { 
-    try{ 
+  };
+
+  const handleSubmit = async (e) => {
+    try {
       e.preventDefault();
-      const result = await axios.post("http://localhost:8080", {
-        image: image
-      })
+      const result = await axios.post("http://localhost:8080/upload", {
+        image: image,
+      });
       const uploadedIMG = result.data.public_id;
-      window.location.reload() // set this to navigate to homepage plus image uploaded pop up box.
-    }catch(err){
-      console.log(err)
+      window.location.reload(); // set this to navigate to homepage plus image uploaded pop up box.
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
   return (
     <>
-    <div className="container mt-5 align-items-center justify-content-center">
-    <form onSubmit={e=> handleSubmit(e)}>
-        <label htmlFor= "fileInput"> Upload your photo here</label>
-          <input type="file" id="fileInput" onChange={e=> handleChange(e)} required  
-          accept="image/png, image/jpeg, image/jpg, image/jfif"/> 
+      <div className="container mt-5 align-items-center justify-content-center">
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <label htmlFor="fileInput"> Upload your photo here</label>
+          <input
+            type="file"
+            id="fileInput"
+            onChange={(e) => handleChange(e)}
+            required
+            accept="image/png, image/jpeg, image/jpg, image/jfif"
+          />
           <button className="btn btn-primary">submit </button>
-    </form>
-    </div>
-    <img src={image} alt="" /> 
+        </form>
+      </div>
+      <img src={image} alt="" />
     </>
-)
+  );
 };
 
 export default Upload;
