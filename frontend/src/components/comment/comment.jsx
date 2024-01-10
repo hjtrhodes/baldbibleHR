eff3e2a34dd1c88863a8703ac445797f1fc09bf3
Harry Rhodes <hjtrhodes@gmail.com>
2024-01-10 20:48:03 +0000

    Comments fully functionaly with redirect to login if no valid token and alert

--- frontend/src/components/comment/comment.jsx
+++ frontend/src/components/comment/comment.jsx
@@ -23,33 +23,47 @@ const Comments = ({ imageId }) => {
 
   const handleCommentSubmit = async (e) => {
     e.preventDefault();
-
+  
     try {
+      const token = window.localStorage.getItem("token");
+  
+      // Check if the token is available
+      if (!token) {
+        // Token is missing, ask user if they want to go to the login page
+        const goToLoginPage = window.confirm("You need to be logged in to add a comment. Do you want to go to the login page?");
+        
+        if (goToLoginPage) {
+          // Redirect to the login page
+          window.location.href = `/login`;
+          return; // Stop further execution
+        } else {
+          // User decided not to go to the login page
+          return;
+        }
+      }
+  
       // Send a request to add a new comment
-     
       const url = `http://localhost:8080/api/images/${imageId}/comments`;
       const body = {
-                      content: newComment,
-                };
+        content: newComment,
+      };
       const options = {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       };
-
-      await axios.post(url, body, options)
-
-      
-
+  
+      await axios.post(url, body, options);
+  
       // Refresh the comments after submitting a new comment
       const response = await axios.get(`http://localhost:8080/api/images/${imageId}/comments`, {
-        'headers' : {
-          'Authorization': `Bearer ${token}`
-        }
-      })
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       setComments(response.data);
-      console.log(response.data)
-
+      console.log(response.data);
+  
       // Clear the input field
       setNewComment('');
     } catch (error) {
