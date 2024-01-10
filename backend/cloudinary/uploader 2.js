539a78134f61c2fa0fc503b10b172bef3b3fe32a
Harry Rhodes <hjtrhodes@gmail.com>
2024-01-10 21:03:54 +0000

    changing branch comments merged with main

--- backend/cloudinary/uploader 2.js
+++
@@ -1,5 +0,0 @@
-const cloudinary = require("./cloudinary")
-
-cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
