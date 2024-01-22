b48a242e78c20bf42dbca9e4c53a3526e64267e0
Harry Rhodes <hjtrhodes@gmail.com>
2024-01-22 11:08:40 +0000

    tried changing baseurl back

--- frontend/util/baseUrl.js
+++ frontend/util/baseUrl.js
@@ -1,11 +1,6 @@
-let baseUrl;
+const baseUrl = "http://localhost:8080"
 
-if (process.env.NODE_ENV === 'production') {
-  // Set the deployed URL
-    baseUrl = "https://baldbible-backend-w85i.onrender.com:8080";
-} else {
-  // Set the local URL
-    baseUrl = "http://localhost:8080";
-}
+export default baseUrl
 
-export default baseUrl;
+
+//https://baldbible-backend-w85i.onrender.com
