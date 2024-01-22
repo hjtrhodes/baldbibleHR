784be12906c47e54e1fb5f95a68bf85cedbb3642
Harry Rhodes <hjtrhodes@gmail.com>
2024-01-22 11:09:34 +0000

    not basURL

--- frontend/util/baseUrl.js
+++ frontend/util/baseUrl.js
@@ -1,6 +1,11 @@
-const baseUrl = "http://localhost:8080"
+let baseUrl;
 
-export default baseUrl
+if (process.env.NODE_ENV === 'production') {
+  // Set the deployed URL
+    baseUrl = "https://baldbible-backend-w85i.onrender.com:8080";
+} else {
+  // Set the local URL
+    baseUrl = "http://localhost:8080";
+}
 
-
-//https://baldbible-backend-w85i.onrender.com
+export default baseUrl;
