ec361003cb7462b7d4c5d70b2cede020a76ba20d
Harry Rhodes <hjtrhodes@gmail.com>
2024-01-23 12:55:14 +0000

    updated render message

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,69 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Feed from '../components/feed/Feed';
 import './Home.css';
 
 const Home = ({ navigate }) => {
-    const [showMessage, setShowMessage] = useState(true);
-    const [timer, setTimer] = useState(() => {
-        // Try to get timer value from local storage, or set default to 300 seconds
-        return parseInt(localStorage.getItem('timer')) || 300;
-    });
+    const [showLongMessage, setShowLongMessage] = useState(false);
 
-    useEffect(() => {
-        let interval;
-
-        if (showMessage && timer > 0) {
-            interval = setInterval(() => {
-                setTimer((prevTimer) => {
-                    // Save timer value to local storage
-                    localStorage.setItem('timer', prevTimer - 1);
-                    return prevTimer - 1;
-                });
-            }, 1000);
-        }
-
-        return () => {
-            clearInterval(interval);
-        };
-    }, [showMessage, timer]);
-
-    useEffect(() => {
-        if (timer === 0) {
-            setShowMessage(false);
-        }
-    }, [timer]);
+    const handleToggleMessage = () => {
+        setShowLongMessage(!showLongMessage);
+    };
 
     return (
-        <>
         <div>
             <div>
-            <h2 className="mainHeader" id="home-statement">
-                Welcome to Bald Bible
-        </h2>
-            <p className="mainTagline" id="home-statement">
-                A positive corner of the internet to help you embrace your hair loss and thrive.
-            </p>
-            {showMessage && (
-            <div className='rendercontainer'>
-                <>
-                <p className='rendermessage'>
-                    This website is deployed as a free web service on Render.com - These web
-                    services spin down with inactivity, particularly the backend server, so if you are not seeing any pictures and
-                    can't login or signup, you may have to wait up to five minutes for it to wake up! Why not go grab a cup of
-                    tea and come back? I promise it is worth it. But if you can see pictures - ignore this message - the server is running, you're good to go!
+                <h2 className="mainHeader" id="home-statement">
+                    Welcome to Bald Bible
+                </h2>
+                <p className="mainTagline" id="home-statement">
+                    A positive corner of the internet to help you embrace your hair loss and thrive.
                 </p>
-                <p className='rendertimer'>
-                    Time remaining: {Math.floor(timer / 60)}:{(timer % 60).toString().padStart(2, '0')}
-                </p>
-                </>
-            </div>
-            )}
+                {!showLongMessage && (
+                        <>
+                            <p className='rendermessage'>
+                            </p>
+                            <button className='showmessagebutton' onClick={handleToggleMessage}>Can't see any pictures? - Click Here</button>
+                        </>
+                )}
+                {showLongMessage && (
+                    <div className='rendercontainer'>
+                        <>
+                            <p className='rendermessage'>
+                                This website is deployed as a free web service on Render.com
+                            </p>
+                            <p className='rendermessage'>
+                                These web services spin down with inactivity, so if you are not seeing any pictures and
+                                can't login or signup, you may have to wait a few minutes for the backend server to wake up. Sorry about that, but why not take a well-earned break, go grab a cup of
+                                tea and come back?
+                            </p>
+                            <p className='rendermessage'>
+                                If you can see pictures but clicked the button anyway - ignore this message - the server is running, you're good to go!
+                            </p> 
+                            <button className='messagebutton' onClick={handleToggleMessage}>Hide Message</button>
+                        </>
+                    </div>
+                )}
             </div>
             <Feed navigate={navigate} />
         </div>
-        </>
     );
 };
 
-
 export default Home;
+
+
